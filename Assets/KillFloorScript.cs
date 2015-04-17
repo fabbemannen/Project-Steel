63eb155f90a84bbdbec28ff2feb894d4713c5d70
Carveca <carveca2@gmail.com>
2015-04-17 13:20:07 +0200

    Added respawn and goal

---
+++ Assets/KillFloorScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillFloorScript : MonoBehaviour 
+{
+    public Transform startPosition;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = startPosition.transform.position;
+        other.transform.rotation = startPosition.transform.rotation;
+        other.transform.Rotate(Vector3.zero);
+        other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
